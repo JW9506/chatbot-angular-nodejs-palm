12e78e39ac3adde7f745e86b5dca45468b6f5c03
JW9506 <cf.dota@gmail.com>
2023-07-15 14:14:02 -0500

    Run server on 5050

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ require('dotenv').config()
 const { DiscussServiceClient } = require("@google-ai/generativelanguage");
 const { GoogleAuth } = require("google-auth-library");
 
+const app = express();
+const port = 5050;
 const API_KEY = process.env.API_KEY;
 
 const client = new DiscussServiceClient({
@@ -19,4 +21,8 @@ app.use(
 );
 
 app.post("/api/chatbot", async (req, res) => {
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
