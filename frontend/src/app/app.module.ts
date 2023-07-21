9a1cfc63f8349976593519e4bb7133c526c3803d
JW9506 <cf.dota@gmail.com>
2023-07-21 01:15:36 -0500

    Add message pipe to prettify message output

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { LineBreakPipe } from './pipe/linebreak.pipe';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    LineBreakPipe,
   ],
   providers: [],
   bootstrap: [AppComponent]
